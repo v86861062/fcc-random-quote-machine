63fa6ff1d4657938a3072c5102635cf13751b4df
v86861062 <v86861062@gmail.com>
2018-10-29 13:30:19 +0800

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,39 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import { CSSTransition } from 'react-transition-group';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { fab, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import React, { Component } from "react"
+import "./App.css"
+import { CSSTransition } from "react-transition-group"
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { fab, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import colors from "./colors"
 
 library.add(fab, faTwitter)
-
-var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-              '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-              '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-              '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-              '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-              '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-              '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-              '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-              '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-              '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF']
-
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       error: null,
       isLoaded: false,
       showQuote: false,
       quotesData: [],
-      quote: '',
-      quoteChinese: '',
-      author: '',
-      color: '#ffe4c4'
+      quote: "",
+      quoteChinese: "",
+      author: "",
+      color: "#ffe4c4"
     }
   }
 
-  getRandomArrayItem = (arr) => {
+  getRandomArrayItem = arr => {
     return arr[Math.floor(Math.random() * arr.length)]
   }
 
@@ -46,21 +35,21 @@ class App extends Component {
     return this.getRandomArrayItem(colors)
   }
 
-  handleClick = (e) => {    
-    this.setState(state => ({
+  handleClick = () => {
+    this.setState({
       showQuote: false,
       color: this.getRandomColor()
-    }))
+    })
   }
 
-  changeQuote = (e) => {
+  changeQuote = () => {
     let randomQuote = this.getRandomQuote()
-    this.setState(state => ({
+    this.setState({
       quote: randomQuote.quote,
       quoteChinese: randomQuote.quote_zh_tw,
       author: randomQuote.author,
       showQuote: true
-    }))
+    })
   }
 
   componentDidMount() {
@@ -68,68 +57,86 @@ class App extends Component {
     fetch(nowUrl + "/quotes.json")
       .then(res => res.json())
       .then(
-        (result) => {
+        result => {
           this.setState({
             isLoaded: true,
             quotesData: result
-          });
+          })
           this.changeQuote()
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
-        (error) => {
+        error => {
           this.setState({
             isLoaded: true,
-            error 
-          });
+            error
+          })
         }
       )
   }
 
   render() {
-      const { error, isLoaded, showQuote,
-              quote, quoteChinese, author} = this.state
+    const {
+      error,
+      isLoaded,
+      showQuote,
+      quote,
+      quoteChinese,
+      author
+    } = this.state
 
-      const stylesObj = {
-        background: this.state.color
-      };
+    const stylesObj = {
+      background: this.state.color
+    }
 
-      if (error) {
-        return <div id="error">Error: {error.message}</div>
-      } else if (!isLoaded) {
-        return <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-      } else {
-        let tweetShareURL = "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
-                        encodeURIComponent(quote + author)
-        return (
-          <div className="container" style={stylesObj}>
-            <div  id="quote-box">
-              <CSSTransition
-                in={showQuote}
-                timeout={500}
-                classNames="quote"
-                onExited={() => {
-                  this.changeQuote()
-                }}> 
-                  {state => (
-                    <div>
-                      <div id="text">{quote}</div>
-                      <div id="text-chinese">{quoteChinese}</div>
-                      <div id="author">-{author}</div>
-                    </div>
-                  )}
-                </CSSTransition>
+    if (error) {
+      return <div id="error">Error: {error.message}</div>
+    } else if (!isLoaded) {
+      return (
+        /* 讀取圈圈效果 */
+        <div className="lds-ring">
+          <div />
+          <div />
+          <div />
+          <div />
+        </div>
+      )
+    } else {
+      let tweetShareURL =
+        "https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=" +
+        encodeURIComponent(quote + author)
 
-              <button id="new-quote" onClick={this.handleClick}>new quote</button>
-              <a id="tweet-quote" href={tweetShareURL}>
-                <FontAwesomeIcon icon={['fab', 'twitter']} />
-              </a>
+      return (
+        <div className="container" style={stylesObj}>
+          <div id="quote-box">
+            <CSSTransition
+              in={showQuote}
+              timeout={500}
+              classNames="quote"
+              onExited={() => {
+                this.changeQuote()
+              }}
+            >
+              <div>
+                <div id="text">{quote}</div>
+                <div id="text-chinese">{quoteChinese}</div>
+                <div id="author">-{author}</div>
               </div>
+            </CSSTransition>
+
+            <button id="new-quote" onClick={this.handleClick}>
+              new quote
+            </button>
+
+            <a id="tweet-quote" href={tweetShareURL}>
+              <FontAwesomeIcon icon={["fab", "twitter"]} />
+            </a>
           </div>
-        )
-      }
+        </div>
+      )
+    }
   }
 }
 
-export default App;
+export default App
